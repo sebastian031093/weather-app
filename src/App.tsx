a2d931916528e161fa6d37cca1401467cc3f67e7
Sebastian Manco G <sebasmg1393@gmail.com>
2025-07-27 18:02:51 -0500

    compenets integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
-import './App.css';
 import SearchBar from './components/SearchBar';
+import { Container, Typography } from '@mui/material';
+import WeatherCard from './components/WeatherCard';
+import { fetchWeather } from './services/weatherApi';
+import './App.css';
 
 function App() {
-  //const [count, setCount] = useState(0);
+  const [weather, setWeather] = useState<any>(null);
+  const [error, setError] = useState('');
 
-  function hadleSearch(params: string) {
+  function handleSearch(params: string) {
     console.log('aqui van lo que buscas', params);
   }
 
   return (
-    <>
-      <h1>Hi from Weather app</h1>
-
-      <SearchBar onSearch={hadleSearch} />
-    </>
+    <Container>
+      <Typography variant="h3" textAlign="center" mt={4}>
+        Weather App
+      </Typography>
+      <SearchBar onSearch={handleSearch} />
+      {error && <Typography color="error">{error}</Typography>}
+      {weather && <WeatherCard data={weather} />}
+    </Container>
   );
 }
 
